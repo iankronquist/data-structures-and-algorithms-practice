616ef394295dbe87b476d2fcf002085c9efe650e
Ian Kronquist <iankronquist@teleport.com>
2014-11-21 12:01:49 -0800

    Add a simple heapsort implementation

--- heapsort.c
+++ heapsort.c
@@ -10,7 +10,6 @@ void sort_heap(int *heap,  int heapl);
 void sift_down(int *heap, int heapl, int index);
 void swap(int *heap, int a, int b);
 void print_heap(int *heap, int heapl);
-
 int main() {
     int heapl = 10;
     int *heap = malloc(heapl * sizeof(int));
@@ -46,20 +45,67 @@ void verify_heap(int *heap, int heapl) {
 }
 
 void heapify(int *heap, int heapl) {
-    // FIXME
+    for (int i = 0; i < heapl; i++) {
+        sift_down(heap, heapl, i);
+    }
 }
 
 void sort_heap(int *heap,  int heapl) {
-    // FIXME
+    for (int i = heapl/2; i >= 0; i--) {
+        sift_down(heap, i, heapl);
+        print_heap(heap, heapl);
+    }
+    for (int i = heapl-1; i >= 1; i--) {
+        swap(heap, 0, i);
+        sift_down(heap, 0, i-1);
+    }
 }
 
 void swap(int *heap, int a, int b) {
-    // FIXME: Explain why this works
+    printf("swapping %i %i\n", heap[a], heap[b]);
     heap[a] ^= heap[b];
     heap[b] ^= heap[a];
     heap[a] ^= heap[b];
 }
 
 void sift_down(int *heap, int index, int bottom) {
-    // FIXME
+    int childl = index * 2;
+    if (childl > bottom) return;
+    int childr = index * 2 + 1;
+    int follow = index;
+    if (childl == bottom) {
+        follow = childl;
+    } else if (heap[childr] > heap[childl]) {
+        follow = childr;
+    } else {
+        follow = childl;
+    }
+    if (heap[index] < heap[follow]) {
+        swap(heap, index, follow);
+    }
+    if(follow == index) return;
+    sift_down(heap, follow, bottom);
 }
+/*
+void sift_down(int *heap, int root, int bottom)
+{
+  int maxChild, temp;
+
+  while ((root*2 <= bottom))
+  {
+    if (root*2 == bottom)
+      maxChild = root * 2;
+    else if (heap[root * 2] > heap[root * 2 + 1])
+      maxChild = root * 2;
+    else
+      maxChild = root * 2 + 1;
+
+    if (heap[root] < heap[maxChild])
+    {
+      swap(heap, root, maxChild);
+      root = maxChild;
+    }
+    else
+        return;
+  }
+}*/
