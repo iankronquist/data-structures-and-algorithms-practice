fef470f4657cd1563717f4a3952fb4698ed06a1b
Ian Kronquist <iankronquist@teleport.com>
2014-11-21 12:50:03 -0800

    A working quicksort implementation

--- quicksort.c
+++ quicksort.c
@@ -6,17 +6,19 @@
 void seed_arr(int * arr, int arrl);
 void verify_arr(int *arr, int arrl);
 void arrify(int *arr, int arrl);
-void sort_arr(int *arr,  int arrl);
+void quicksort(int *arr,  int begin, int end);
 void sift_down(int *arr, int arrl, int index);
 void swap(int *arr, int a, int b);
 void print_arr(int *arr, int arrl);
+int partition(int *arr, int begin, int end);
+int choose_pivot(int begin, int end);
 
 int main() {
     int arrl = 10;
     int *arr = malloc(arrl * sizeof(int));
     seed_arr(arr, arrl);
     print_arr(arr, arrl);
-    sort_arr(arr, arrl);
+    quicksort(arr, 0, arrl-1);
     print_arr(arr, arrl);
     verify_arr(arr, arrl);
     free(arr);
@@ -45,13 +47,42 @@ void verify_arr(int *arr, int arrl) {
     }
 }
 
-void sort_arr(int *arr,  int arrl) {
-    // FIXME
+void quicksort(int *arr,  int begin, int end) {
+    int pivot;
+    if (begin < end) {
+        pivot = partition(arr, begin, end);
+        quicksort(arr, begin, pivot - 1);
+        quicksort(arr, pivot + 1 , end);
+    }
+}
+
+int partition(int *arr, int begin, int end) {
+    int pivot = choose_pivot(begin, end);
+    int pivot_val = arr[pivot];
+    swap(arr, pivot, end);
+    int store_index = begin;
+    for (int i = begin; i < end; i++) {
+        if (arr[i] < pivot_val) {
+            swap(arr, i, store_index);
+            store_index++;
+        }
+    }
+    swap(arr, store_index, end);
+    return store_index;
+}
+
+int choose_pivot(int begin, int end) {
+    // TODO: implement median choice, or random choice
+    return (end-begin)/2 + begin;
 }
 
 void swap(int *arr, int a, int b) {
-    // FIXME: Explain why this works
+    int tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+    /*
     arr[a] ^= arr[b];
     arr[b] ^= arr[a];
     arr[a] ^= arr[b];
+    */
 }
